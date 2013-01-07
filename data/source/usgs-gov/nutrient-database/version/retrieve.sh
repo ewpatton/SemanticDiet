c62c6337569085446676a909ba85537d9e8b9c84
Evan W. Patton <ewpatton@gmail.com>
2013-01-07 10:01:39 -0500

    changed retrieve.sh to use substitution syntax

--- data/source/usgs-gov/nutrient-database/version/retrieve.sh
+++ data/source/usgs-gov/nutrient-database/version/retrieve.sh
@@ -12,8 +12,8 @@
 #3>      <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Automated-creation-of-a-new-Versioned-Dataset>;
 #3> .
 
-DATASET:="http://www.ars.usda.gov/SP2UserFiles/Place/12354500/Data/SR25/dnload/sr25.zip"
-VERSION:="release-25"
+DATASET=${DATASET:-"http://www.ars.usda.gov/SP2UserFiles/Place/12354500/Data/SR25/dnload/sr25.zip"}
+VERSION=${VERSION:-"release-25"}
 
 export CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER="true"
 $CSV2RDF4LOD_HOME/bin/cr-create-versioned-dataset-dir.sh $VERSION $DATASET
